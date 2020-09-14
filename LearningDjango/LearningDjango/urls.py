70b17a2a1c91f2f7effcab548b9345f3248dbdb0
Scott Gordon <SAGordon@pythonpro.dev>
2020-09-14 16:50:09 -0400

    changes

--- LearningDjango/LearningDjango/urls.py
+++ LearningDjango/LearningDjango/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
+from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 
 urlpatterns = [
@@ -27,4 +29,6 @@ urlpatterns = [
          name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
