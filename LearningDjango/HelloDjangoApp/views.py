7f9dc5ca6db16e94a4aa5d5e0fa2f8cd7f8375c4
Scott Gordon <SAGordon@pythonpro.dev>
2020-09-16 16:44:45 -0400

    Add a page to the app

--- LearningDjango/HelloDjangoApp/views.py
+++ LearningDjango/HelloDjangoApp/views.py
@@ -6,14 +6,19 @@ from django.shortcuts import render
 def index(request):
     now = datetime.now()
 
-    return render(
-        request,
+    return render(request,
         "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
         # "index.html", # Use this code for VS 2017 15.7 and earlier
         {
             'title' : "Hello Django",
             'message' : "Hello Django",
             'content': " on " + now.strftime("%A, %d %B, %Y at %X")
-        }
-    )
+        })
 
+def about(request):
+    return render(request,
+        "HelloDjangoApp/about.html",
+        {
+            'title' : "About HelloDjangoApp",
+            'content' : "Example app page for Django."
+            })
--- LearningDjango/LearningDjango/urls.py
+++ LearningDjango/LearningDjango/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', HelloDjangoApp.views.index, name='index'),
     url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^about$', HelloDjangoApp.views.about, name='about'),
 ]
